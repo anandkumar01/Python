f13ac7c2ea628df39ce3ca2ab1588bdb11c44518
anandkumar01 <vishwakarmaanand858@gmail.com>
2025-02-26 11:04:58 +0530

    Subarray with maximum sum

--- SlidingWindow/MaxSumSubarray.py
+++ SlidingWindow/MaxSumSubarray.py
@@ -13,6 +13,26 @@ def maxsumsubarray(nums):
             cur_sum = 0
     return max_sum
 
+# Return subarray with maximum sum
+def subarray_with_maxsum(nums):
+    if not nums:
+        return []
+    max_sum = float("-inf")
+    cur_sum = float("-inf")
+    start, end = 0, 0
+    w_start = 0
+    for w_end in range(len(nums)):
+        cur_sum += nums[w_end]
+        if cur_sum > max_sum:
+            max_sum = cur_sum
+            start = w_start
+            end = w_end
+        if cur_sum < 0:
+            cur_sum = 0
+            w_start = w_end + 1
 
-nums = [2, -5, 1, 3, 7, -3, 2]
-print(maxsumsubarray(nums))
+    return nums[start : end + 1]
+
+
+nums = [2, -5, 1, 3, 7, -3, 2, 7]
+print(subarray_with_maxsum(nums))
